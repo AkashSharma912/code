5fcfea8c872e1350cc2121ead20aeb2c74d81237
Akash Sharma <akasharma912@gmail.com>
2025-05-31 13:12:55 +0530

    optimization

--- Dynamic Programming/L10. Min Sum Path.cpp
+++ Dynamic Programming/L10. Min Sum Path.cpp
@@ -98,3 +98,31 @@ public:
 };
 
 X-----------------------------------------------X-------------------------------------X-
+
+// recursion
+class Solution {
+public:
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+         vector<int> prev(m,0);
+         for(int i=0;i<=n-1;i++){
+             vector<int>curr(m,0);
+             for(int j=0;j<=m-1;j++){
+                 if(i==0 and j==0)curr[0]=grid[0][0];
+                else{
+                 int up=grid[i][j];
+                 int left=grid[i][j];
+                 if(i>0)up+=prev[j];
+                 else up+=1e9;
+                 if(j>0) left+=curr[j-1];
+                 else left+=1e9;
+                 curr[j]=min(up,left);
+                }
+             }
+             prev=curr;
+         }
+         return prev[m-1];
+    }
+};
