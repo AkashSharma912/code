a2c5f0c22b7cfb83f7fd1a3c2b7a060d157b4819
Akash Sharma <akasharma912@gmail.com>
2025-05-31 13:11:30 +0530

    D.P. series 31-05-2025

---
+++ Dynamic Programming/L10. Min Sum Path.cpp
@@ -0,0 +1,100 @@
+X-------------------------------X-------------------------------------X-
+
+RECURSION
+class Solution {
+public: 
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        return f(n-1,m-1,grid);
+    }
+    int f(int i, int j , vector<vector<int>>& a)
+    {
+        if(i ==0 && j==0)
+        return a[i][j];
+
+        if(i<0 || j<0)
+        return 1e9;
+
+        int up = a[i][j] + f(i-1,j,a);
+        int left= a[i][j] + f(i,j-1,a);
+
+        return min(up,left);
+    }
+};
+
+
+
+X=========================X================================================X===========
+
+MEMOIZATION
+
+class Solution {
+public: 
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        
+        return f(n-1,m-1,grid,dp);
+    }
+    int f(int i, int j , vector<vector<int>>& a,vector<vector<int>> &dp)
+    {
+        if(i ==0 && j==0)
+        return a[i][j];
+
+        if(i<0 || j<0)
+        return 1e9;
+
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+
+        int up = a[i][j] + f(i-1,j,a,dp);
+        int left= a[i][j] + f(i,j-1,a,dp);
+
+        return dp[i][j]=min(up,left);
+    }
+};
+
+x-------------------------x-------------------------------x
+
+TABULATI0N
+
+class Solution {
+public: 
+    int minPathSum(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
+
+        for(int i =0 ;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 & j==0)
+                dp[i][j]=a[0][0];
+                else 
+                {
+                    int up=a[i][j];
+                    if(i>0)
+                     up +=dp[i-1][j];
+                    else
+                    up+=1e9;
+                    int left = a[i][j] ;
+                   if(j>0) left+= dp[i][j-1];
+                   else
+                   left+=1e9;
+
+
+                   dp[i][j] = min(up,left);
+
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+};
+
+X-----------------------------------------------X-------------------------------------X-
